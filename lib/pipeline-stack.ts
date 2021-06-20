afb9dd0df225b4b8d3763074bffc46f9419750d1
Philip Damra <pdamra@dealerinspire.com>
2021-06-20 09:23:21 -0700

    Add the artifacts to hold the source code and the synth artifacts

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,4 +1,6 @@
 import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import { Artifact } from '@aws-cdk/aws-codepipeline';
+import { CdkPipeline } from '@aws-cdk/pipelines';
 
 export interface PipelineStackProps extends StackProps {
   // The branch we will be deploying from
@@ -18,10 +20,35 @@ export interface PipelineStackProps extends StackProps {
 }
 
 export class PipelineStack extends Stack {
+  // Reference to the passed-in props
+  private props: PipelineStackProps;
+
+  // The output artifact created by the source action (code from the repo)
+  private sourceArtifact: Artifact;
+
+  // The output artifact from the synth action (holds the cdk.out directory)
+  private cloudAssemblyArtifact: Artifact;
+
+  // The pipeline that runs the operations
+  private pipeline: CdkPipeline;
+
+  // Pipeline name
+  private pipelineName: string;
+
   constructor(scope: Construct, id: string, props: PipelineStackProps) {
     super(scope, id, props);
+    this.props = props;
+    this.pipelineName = `${props.repo}-${props.stageName}`;
+
     this.buildResources();
   }
 
-  private buildResources() {}
+  private buildResources() {
+    this.buildArtifacts();
+  }
+
+  private buildArtifacts() {
+    this.sourceArtifact = new Artifact(`${this.pipelineName}-sourceArtifact`);
+    this.cloudAssemblyArtifact = new Artifact(`${this.pipelineName}-cloudAssemblyArtifact`);
+  }
 }
