b5e508fdb77364411f016d5aeedb32c5bef1047c
Philip Damra <pdamra@dealerinspire.com>
2021-06-21 23:41:17 -0700

    Add AppSync API and graphql schema

--- lib/appsync-notes-stack.ts
+++ lib/appsync-notes-stack.ts
@@ -1,9 +1,34 @@
-import * as cdk from '@aws-cdk/core';
+import { AuthorizationType, GraphqlApi, Schema } from '@aws-cdk/aws-appsync';
+import { CfnOutput, Construct, Duration, Expiration, Stack, StackProps } from '@aws-cdk/core';
 
-export class AppsyncNotesStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class AppsyncNotesStack extends Stack {
+  public apiUrl: string;
+  public apiKey: string;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new GraphqlApi(this, 'AppSyncNotesApi', {
+      name: 'appsync-notes-api',
+      schema: Schema.fromAsset('graphql/schema.graphql'),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: Expiration.after(Duration.days(365)),
+          },
+        },
+      },
+    });
+    this.apiUrl = api.graphqlUrl;
+    this.apiKey = api.apiKey || '';
+
+    new CfnOutput(this, 'AppSyncNotesApiUrl', {
+      value: this.apiUrl,
+    });
+
+    new CfnOutput(this, 'AppSyncNotesApiKey', {
+      value: this.apiKey,
+    });
   }
 }
